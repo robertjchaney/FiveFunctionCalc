c78697fc2b66eb83e9f116ba1b3655b9269e88bf
robertjchaney <rchaney8384@stu.neisd.net>
2023-08-16 11:39:55 -0500

    8/16/2023

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
+// import * package
+import java.util.*;
+
 class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Scanner user = new Scanner(System.in);
+        System.out.println("Welcome to Five Function Calculator. This calculator can perform the following: \n Addition\n Subtraction\n Multiplication\n Division\n Modulus");
     }
 }
